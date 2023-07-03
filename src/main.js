84ca71cf6cbc27093b5a1bc011d0f11eb07dfa79
markvayson <markvayson@gmail.com>
2023-07-03 18:43:38 +0400

    latest

--- src/main.js
+++ src/main.js
@@ -2,111 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const choices = ['rock', 'paper', 'scissor'];
     let isPlaying = false;
     
-    const npcStartingWords = [
-        "Prepare yourself, human. Whiskerclaw is here to test your skills!",
-        "Meow-velous! Let the battle commence between us, mortal.",
-        "In the realm of Rock, Paper, Scissors, Whiskerclaw reigns supreme!",
-        "Your feeble attempts won't be enough to defeat me, hooman. Let's see what you've got!",
-        "Ah, another challenger. The Clash of Catitude begins now!"
-    ];
-
-    const userDrawPhrase = [
-        "Whiskerclaw, we may be evenly matched, but I won't back down. Prepare for our next encounter!",
-        "A draw, Whiskerclaw. Our battles are always intense. I look forward to our next clash of wits!",
-        "No winner this time, Whiskerclaw. Our rivalry grows stronger with each round. Until we meet again!",
-        "Well fought, Whiskerclaw. Neither of us could claim victory, but our rivalry continues to fuel my determination.",
-        "Whiskerclaw, the battle ended in a draw, but our rivalry is far from over. Prepare yourself for the next round!"
-      ];
-      
-      
-
-    const npcDrawPhrase = [
-        "A draw? Don't get too confident, human. I'll get you next time.",
-        "A stalemate, but I won't settle for it. Prepare for your ultimate defeat!",
-        "Impressive, but luck won't save you forever. I'll have my revenge soon.",
-        "We're evenly matched, for now. But mark my words, I'll triumph in the end.",
-        "A draw? This battle is far from over. I'll keep honing my claws for our next encounter."
-      ];
-      
-    const userStartingWords = [
-        "Prepare yourself, Whiskerclaw! I'm ready to take you on!",
-    "Whiskerclaw, your reign ends here! Let's see who triumphs!",
-    "I won't back down, Whiskerclaw! Get ready to face defeat!",
-    "Whiskerclaw, your claws may be sharp, but my strategy is sharper!",
-    "Time to show Whiskerclaw who the true champion is!"
-    ];
-
-    const npcWinning = [[
-        "Meow-velous! I've claimed the first victory. Don't get too comfortable, human.",
-        "You've fallen into my feline trap. The battle has just begun!",
-        "Hiss! The superior claws of Whiskerclaw have prevailed in this round."
-       ],[
-        "I'm one step closer to victory. Your defeat is inevitable, human.",
-        "You're no match for my calculated moves. The tide turns in my favor!",
-        "Purrfection! I've outsmarted you once again. Can you handle the pressure?"
-        ],[
-        "You're struggling to keep up, human. My victory is within paw's reach!",
-        "I've clawed my way to another triumph. Your defeat grows nearer.",
-        "Your feeble attempts amuse me. Prepare for a true display of my power!"
-        ],[
-        "You're no match for my superior strategy. Your defeat is all but certain.",
-        "You're running out of luck, human. The scales tip irreversibly in my favor!",
-        "You're at my mercy. Your feeble attempts are like a mouse before a lion!"
-        ],[
-        "No! The feline foe emerges triumphant once again. You're no match for Whiskerclaw!",
-        "You've put up a valiant fight, but my supremacy remains unchallenged. Bow before me!",
-        "Hiss and growl! You've fallen short of victory. I am the true conqueror of the battlefield!"]
-    ];
-
-    const npcLosing = [[
-        "You think you stand a chance against me? Prepare to be crushed, puny human!",
-    "I am Whiskerclaw, the unbeatable feline master! Tremble before my claws!",
-    "Your feeble attempts to defeat me are laughable! I am the true champion of this game!"],[
-        "Did you really think you could outsmart me? I am Whiskerclaw, the ultimate gaming genius!",
-"Your defeat is imminent, as I am the superior adversary! Behold the might of Whiskerclaw!",
-"Pathetic human, you cannot comprehend the level of skill possessed by Whiskerclaw!"
-    ],["My victories over you are a testament to my superior strategy and intellect. Bow down to Whiskerclaw!",
-    "You're no match for my cunning, human! Accept your defeat at the paws of Whiskerclaw!",
-    "Whiskerclaw always prevails! Your defeat is merely another step in my path to glory!"
-
-    ],["Your futile attempts to defeat me are amusing, but ultimately useless. Whiskerclaw remains supreme!",
-    "The more you struggle, the deeper you fall into the clutches of Whiskerclaw's unbeatable prowess!",
-    "Your defeat is inevitable, for I am Whiskerclaw, the indomitable champion of this game!"],
-    ["Witness the culmination of your failures, human! Whiskerclaw's victory is absolute and undeniable!",
-    "You are but a pawn in Whiskerclaw's game, destined to be defeated by my superior skills!",
-    "As the final round approaches, know that your defeat is inevitable. Prepare to be forever humbled by Whiskerclaw!"
-    ]];
-
-    const userWinning = [[
-        "Whiskerclaw, I've only just begun! Prepare for a taste of true defeat!",
-        "Your feeble attempts are laughable, Whiskerclaw. Bow before your superior!",
-        "The first round is mine, Whiskerclaw. Tremble in the face of my unwavering prowess!"],
-        [
-        "Whiskerclaw, your hopes dwindle with each passing round. Surrender now and save yourself the embarrassment!",
-        "Behold, Whiskerclaw, as I dominate yet again! Your fate is sealed, and victory is mine!",
-        "Resistance is futile, Whiskerclaw. The more I win, the more I prove your insignificance!"
-    ],[
-        "Whiskerclaw, your defeat is becoming routine. Prepare to witness true humiliation in each round!",
-        "Pathetic, Whiskerclaw! My superiority grows with every victory. Your defeat is inescapable!",
-        "Round after round, Whiskerclaw, you face nothing but defeat at my hands. Accept your fate!"
-    ],[
-        "Whiskerclaw, the tides have turned against you. You're no match for my relentless determination!",
-        "I revel in your defeat, Whiskerclaw! Each round brings you closer to the realization of your inferiority!",
-        "Whiskerclaw, the more I conquer, the more your spirit breaks. Prepare for an eternity of defeat!"
-    ],[
-        "Whiskerclaw, this is your final chance to surrender. Accept your defeat and acknowledge my unrivaled supremacy!",
-        "Behold, Whiskerclaw, the indomitable force that crushes you in every round. Your defeat is complete!",
-        "As the rounds pass, Whiskerclaw, your defeat is etched into history. Tremble before the victor!"
-    ]];
-
-    const userLosing = [
-        ["Better luck next time!", "Oops! Whiskerclaw got the best of me.", "I'll need to step up my game to defeat Whiskerclaw."],
-        ["Whiskerclaw is a tough opponent, but I won't give up!", "I may have lost this round, but I'll come back stronger!", "I'll learn from my mistakes and come back stronger!"],
-        ["Whiskerclaw's skills are impressive, but I won't give up!", "I'll train harder to defeat Whiskerclaw next time!", "Even though I lost, I'm determined to win the next round!"],
-        ["This round didn't go as planned, but I'll bounce back!", "I'll strategize and come back stronger in the next round!", "Whiskerclaw got lucky this time, but I won't let that discourage me!"],
-        ["Whiskerclaw's victory won't dampen my spirits! I'll come back stronger!", "I'll analyze my mistakes and improve for the next round!", "Losing this round won't stop me from giving my best in the next one!"]
-      ];
-
+  
 
     let choicesIndex = 0;
     let newIndex = 1;
@@ -132,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const npcPhrase = document.getElementById('npcPhrase');
     const again = document.getElementById('again');
     const scene = document.getElementById('scene');
-
+    const settings = document.getElementById('settings');
     //new
     const menu = document.getElementById('menu');  
     const logo = document.getElementById('logo');
@@ -157,20 +53,23 @@ document.addEventListener("DOMContentLoaded", function() {
             scene.classList.add('hidden');
             play.innerHTML = 'Resume';
             burger.innerHTML = 'X';
+            settings.innerHTML = 'New Game';
         } else {
             burger.classList.add("hidden");
             play.innerHTML = 'Play';
 
         }
-        
-        menu.classList.remove("hidden");
+        if(menu.classList.contains('hidden')){
+
+            menu.classList.remove("hidden");
+        } else {
+            menu.classList.add("hidden");
+            burger.innerHTML = '&#9776;';
+            scene.classList.remove("hidden");
+            
+        }
     });
 
-    function resume() {
-        burger.innerHTML = '&#9776';
-        letsplay();
-    }
-    
     //CREATE 
     const rockImg = document.createElement('img');
     const paperImg = document.createElement('img');
@@ -256,6 +155,7 @@ document.addEventListener("DOMContentLoaded", function() {
     //EVENT LISTENERS
     play.addEventListener('click', startIt);
     again.addEventListener('click', startIt);
+    settings.addEventListener('click', startIt);
     
     function handleBtnClick(event) {
         let computerSelection = getComputerChoice();
@@ -288,10 +188,14 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
 
-    function startIt() {
+    function startIt(event) {
+        let choose = (event.target.innerHTML);
+        if(choose === 'Play'){}
         rockBtn.addEventListener('click', handleBtnClick);    
         paperBtn.addEventListener('click', handleBtnClick);
         scissorBtn.addEventListener('click', handleBtnClick);
+        
+        
         npcWins = 0;
         userWins = 0;
         isPlaying = true;
---
+++ src/words.js
@@ -0,0 +1,104 @@
+const npcStartingWords = [
+    "Prepare yourself, human. Whiskerclaw is here to test your skills!",
+    "Meow-velous! Let the battle commence between us, mortal.",
+    "In the realm of Rock, Paper, Scissors, Whiskerclaw reigns supreme!",
+    "Your feeble attempts won't be enough to defeat me, hooman. Let's see what you've got!",
+    "Ah, another challenger. The Clash of Catitude begins now!"
+];
+
+const userDrawPhrase = [
+    "Whiskerclaw, we may be evenly matched, but I won't back down. Prepare for our next encounter!",
+    "A draw, Whiskerclaw. Our battles are always intense. I look forward to our next clash of wits!",
+    "No winner this time, Whiskerclaw. Our rivalry grows stronger with each round. Until we meet again!",
+    "Well fought, Whiskerclaw. Neither of us could claim victory, but our rivalry continues to fuel my determination.",
+    "Whiskerclaw, the battle ended in a draw, but our rivalry is far from over. Prepare yourself for the next round!"
+  ];
+  
+  
+
+const npcDrawPhrase = [
+    "A draw? Don't get too confident, human. I'll get you next time.",
+    "A stalemate, but I won't settle for it. Prepare for your ultimate defeat!",
+    "Impressive, but luck won't save you forever. I'll have my revenge soon.",
+    "We're evenly matched, for now. But mark my words, I'll triumph in the end.",
+    "A draw? This battle is far from over. I'll keep honing my claws for our next encounter."
+  ];
+  
+const userStartingWords = [
+    "Prepare yourself, Whiskerclaw! I'm ready to take you on!",
+"Whiskerclaw, your reign ends here! Let's see who triumphs!",
+"I won't back down, Whiskerclaw! Get ready to face defeat!",
+"Whiskerclaw, your claws may be sharp, but my strategy is sharper!",
+"Time to show Whiskerclaw who the true champion is!"
+];
+
+const npcWinning = [[
+    "Meow-velous! I've claimed the first victory. Don't get too comfortable, human.",
+    "You've fallen into my feline trap. The battle has just begun!",
+    "Hiss! The superior claws of Whiskerclaw have prevailed in this round."
+   ],[
+    "I'm one step closer to victory. Your defeat is inevitable, human.",
+    "You're no match for my calculated moves. The tide turns in my favor!",
+    "Purrfection! I've outsmarted you once again. Can you handle the pressure?"
+    ],[
+    "You're struggling to keep up, human. My victory is within paw's reach!",
+    "I've clawed my way to another triumph. Your defeat grows nearer.",
+    "Your feeble attempts amuse me. Prepare for a true display of my power!"
+    ],[
+    "You're no match for my superior strategy. Your defeat is all but certain.",
+    "You're running out of luck, human. The scales tip irreversibly in my favor!",
+    "You're at my mercy. Your feeble attempts are like a mouse before a lion!"
+    ],[
+    "No! The feline foe emerges triumphant once again. You're no match for Whiskerclaw!",
+    "You've put up a valiant fight, but my supremacy remains unchallenged. Bow before me!",
+    "Hiss and growl! You've fallen short of victory. I am the true conqueror of the battlefield!"]
+];
+
+const npcLosing = [[
+    "You think you stand a chance against me? Prepare to be crushed, puny human!",
+"I am Whiskerclaw, the unbeatable feline master! Tremble before my claws!",
+"Your feeble attempts to defeat me are laughable! I am the true champion of this game!"],[
+    "Did you really think you could outsmart me? I am Whiskerclaw, the ultimate gaming genius!",
+"Your defeat is imminent, as I am the superior adversary! Behold the might of Whiskerclaw!",
+"Pathetic human, you cannot comprehend the level of skill possessed by Whiskerclaw!"
+],["My victories over you are a testament to my superior strategy and intellect. Bow down to Whiskerclaw!",
+"You're no match for my cunning, human! Accept your defeat at the paws of Whiskerclaw!",
+"Whiskerclaw always prevails! Your defeat is merely another step in my path to glory!"
+
+],["Your futile attempts to defeat me are amusing, but ultimately useless. Whiskerclaw remains supreme!",
+"The more you struggle, the deeper you fall into the clutches of Whiskerclaw's unbeatable prowess!",
+"Your defeat is inevitable, for I am Whiskerclaw, the indomitable champion of this game!"],
+["Witness the culmination of your failures, human! Whiskerclaw's victory is absolute and undeniable!",
+"You are but a pawn in Whiskerclaw's game, destined to be defeated by my superior skills!",
+"As the final round approaches, know that your defeat is inevitable. Prepare to be forever humbled by Whiskerclaw!"
+]];
+
+const userWinning = [[
+    "Whiskerclaw, I've only just begun! Prepare for a taste of true defeat!",
+    "Your feeble attempts are laughable, Whiskerclaw. Bow before your superior!",
+    "The first round is mine, Whiskerclaw. Tremble in the face of my unwavering prowess!"],
+    [
+    "Whiskerclaw, your hopes dwindle with each passing round. Surrender now and save yourself the embarrassment!",
+    "Behold, Whiskerclaw, as I dominate yet again! Your fate is sealed, and victory is mine!",
+    "Resistance is futile, Whiskerclaw. The more I win, the more I prove your insignificance!"
+],[
+    "Whiskerclaw, your defeat is becoming routine. Prepare to witness true humiliation in each round!",
+    "Pathetic, Whiskerclaw! My superiority grows with every victory. Your defeat is inescapable!",
+    "Round after round, Whiskerclaw, you face nothing but defeat at my hands. Accept your fate!"
+],[
+    "Whiskerclaw, the tides have turned against you. You're no match for my relentless determination!",
+    "I revel in your defeat, Whiskerclaw! Each round brings you closer to the realization of your inferiority!",
+    "Whiskerclaw, the more I conquer, the more your spirit breaks. Prepare for an eternity of defeat!"
+],[
+    "Whiskerclaw, this is your final chance to surrender. Accept your defeat and acknowledge my unrivaled supremacy!",
+    "Behold, Whiskerclaw, the indomitable force that crushes you in every round. Your defeat is complete!",
+    "As the rounds pass, Whiskerclaw, your defeat is etched into history. Tremble before the victor!"
+]];
+
+const userLosing = [
+    ["Better luck next time!", "Oops! Whiskerclaw got the best of me.", "I'll need to step up my game to defeat Whiskerclaw."],
+    ["Whiskerclaw is a tough opponent, but I won't give up!", "I may have lost this round, but I'll come back stronger!", "I'll learn from my mistakes and come back stronger!"],
+    ["Whiskerclaw's skills are impressive, but I won't give up!", "I'll train harder to defeat Whiskerclaw next time!", "Even though I lost, I'm determined to win the next round!"],
+    ["This round didn't go as planned, but I'll bounce back!", "I'll strategize and come back stronger in the next round!", "Whiskerclaw got lucky this time, but I won't let that discourage me!"],
+    ["Whiskerclaw's victory won't dampen my spirits! I'll come back stronger!", "I'll analyze my mistakes and improve for the next round!", "Losing this round won't stop me from giving my best in the next one!"]
+  ];
